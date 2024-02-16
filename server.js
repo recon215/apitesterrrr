6f6dfb5ec8d853109aee00165e46b68910a172c2
recon215 <131820476+recon215@users.noreply.github.com>
2024-02-15 22:45:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mysql = require('mysql');
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 const port = process.env.PORT || 3000; // Adjust to use the port provided by Heroku dynamically
-
 const connection = mysql.createConnection({
     host: 'sql5.freesqldatabase.com',
     user: 'sql5683328',
@@ -38,27 +37,22 @@ app.get('/stock', (req, res) => {
             console.error('Error querying database:', error);
             return res.status(500).json({ error: 'Database error' });
         }
-
         // Format the results
         const stocks = results.map(stock => ({
             country: stock.country,
             country_code: stock.country_code,
             slots: stock.slots
         }));
-
         // Send the formatted stock data in the response
         res.json(stocks);
     });
 });
-
 app.post('/upgrade', (req, res) => {
     const { Key, Email, CountryCode } = req.query;
-
     // Check if all required parameters are provided
     if (!Key || !Email || !CountryCode) {
         return res.status(400).json({ error: 'Missing parameters' });
     }
-
     // Check if the key exists
     const keyQuery = 'SELECT * FROM codes WHERE `key` = ?';
     connection.query(keyQuery, [Key], (error, keyResults) => {
@@ -66,24 +60,20 @@ app.post('/upgrade', (req, res) => {
             console.error('Error querying database:', error);
             return res.status(500).json({ error: 'Database error' });
         }
-
         // If key does not exist
         if (keyResults.length === 0) {
             return res.status(403).json({ error: 'Invalid key' });
         }
-
         // Check if the key was used in the last 2 weeks
         const lastUsedDate = new Date(keyResults[0].used_timestamp);
         const twoWeeksAgo = new Date();
         twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
-
         if (lastUsedDate > twoWeeksAgo) {
             // Calculate the date when the key can be used again
             const nextAvailableDate = new Date(lastUsedDate);
             nextAvailableDate.setDate(nextAvailableDate.getDate() + 14);
             return res.status(401).json({ error: `This key cannot be used until ${nextAvailableDate.toDateString()}` });
         }
-
         // Fetch a random invite and address from the specified country's table
         const selectRandomQuery = `SELECT invite, address FROM ${CountryCode}_invites ORDER BY RAND() LIMIT 1`;
         connection.query(selectRandomQuery, (selectError, selectResults) => {
@@ -91,10 +81,8 @@ app.post('/upgrade', (req, res) => {
                 console.error('Error selecting random record from database:', selectError);
                 return res.status(500).json({ error: 'Database error' });
             }
-
             // Assuming there's at least one record in the country's invites table
             const { invite, address } = selectResults[0];
-
             // Email sending function
             function sendEmail(email, invite, address) {
                 // Email content
@@ -108,7 +96,6 @@ app.post('/upgrade', (req, res) => {
                         <p>And your address: ${address}</p>
                     `
                 };
-
                 // Send the email
                 transporter.sendMail(mailOptions, (error, info) => {
                     if (error) {
@@ -118,10 +105,8 @@ app.post('/upgrade', (req, res) => {
                     }
                 });
             }
-
             // Inside your '/upgrade' endpoint after retrieving the random invite and address
             sendEmail(Email, invite, address);
-
             // Update the database to mark the key as used, store the email, and timestamp
             const updateQuery = 'UPDATE codes SET used = 1, email = ?, used_timestamp = CURRENT_TIMESTAMP() WHERE `key` = ?';
             connection.query(updateQuery, [Email, Key], (updateError, updateResults) => {
@@ -130,6 +115,7 @@ app.post('/upgrade', (req, res) => {
                     return res.status(500).json({ error: 'Database error' });
                 }
 
+                res.status(200).json({ message: 'Upgrade successful' });
                 // Delete the invite and address that was sent to the user
                 const deleteQuery = `DELETE FROM ${CountryCode}_invites WHERE invite = ? AND address = ?`;
                 connection.query(deleteQuery, [invite, address], (deleteError, deleteResults) => {
@@ -144,18 +130,12 @@ app.post('/upgrade', (req, res) => {
         });
     });
 });
-
-
-
-
 app.get('/renew', (req, res) => {
     const { Key, email } = req.query;
-
     // Check if all required parameters are provided
     if (!Key || !email) {
         return res.status(400).json({ error: 'Missing parameters' });
     }
-
     // Check if the key exists
     const query = 'SELECT * FROM codes WHERE `key` = ?';
     connection.query(query, [Key], (error, results) => {
@@ -163,29 +143,23 @@ app.get('/renew', (req, res) => {
             console.error('Error querying database:', error);
             return res.status(500).json({ error: 'Database error' });
         }
-
         if (results.length === 0) {
             return res.status(403).json({ error: 'Invalid key' });
         }
-
         const keyInfo = results[0];
         const currentDate = new Date();
         const keyLastUsedDate = new Date(keyInfo.used_timestamp);
-
         // Calculate the difference in milliseconds between the current date and the last used date
         const timeDifference = currentDate.getTime() - keyLastUsedDate.getTime();
         const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
         // Check if the key has been used within the past two weeks (14 days)
         if (daysDifference < 14) {
             // Calculate the date when the key can be used again
             const availableDate = new Date(keyLastUsedDate.getTime() + 14 * 24 * 60 * 60 * 1000);
-
             return res.status(403).json({
                 error: `Key has been used within the past two weeks, please wait until ${availableDate.toISOString().split('T')[0]}`
             });
         }
-
         // Check if the key has not been upgraded yet
         if (!keyInfo.used) {
             // Fetch a random invite and address from your accounts table
@@ -195,10 +169,8 @@ app.get('/renew', (req, res) => {
                     console.error('Error selecting random record from database:', selectError);
                     return res.status(500).json({ error: 'Database error' });
                 }
-
                 // Assuming there's at least one record in the accounts table
                 const { invite, address } = selectResults[0];
-
                 // Email sending function
                 function sendEmail(email, invite, address) {
                     // Email content
@@ -212,7 +184,6 @@ app.get('/renew', (req, res) => {
                             <p>And your new address: ${address}</p>
                         `
                     };
-
                     // Send the email
                     transporter.sendMail(mailOptions, (error, info) => {
                         if (error) {
@@ -222,10 +193,8 @@ app.get('/renew', (req, res) => {
                         }
                     });
                 }
-
                 // Inside your '/renew' endpoint after retrieving the random invite and address
                 sendEmail(email, invite, address);
-
                 // Update the database to update the email associated with the key
                 const updateQuery = 'UPDATE codes SET email = ?, used_timestamp = CURRENT_TIMESTAMP() WHERE `key` = ?';
                 connection.query(updateQuery, [email, Key], (updateError, updateResults) => {
@@ -233,7 +202,6 @@ app.get('/renew', (req, res) => {
                         console.error('Error updating database:', updateError);
                         return res.status(500).json({ error: 'Database error' });
                     }
-
                     res.status(200).json({ message: 'Key renewed successfully', invite, address });
                 });
             });
@@ -242,15 +210,12 @@ app.get('/renew', (req, res) => {
         }
     });
 });
-
 app.get('/info', (req, res) => {
     const { Key } = req.query;
-
     // Check if the key parameter is provided
     if (!Key) {
         return res.status(400).json({ error: 'Missing key parameter' });
     }
-
     // Query the database to retrieve information about the key
     const query = 'SELECT email, used_timestamp FROM codes WHERE `key` = ?';
     connection.query(query, [Key], (error, results) => {
@@ -258,38 +223,29 @@ app.get('/info', (req, res) => {
             console.error('Error querying database:', error);
             return res.status(500).json({ error: 'Database error' });
         }
-
         // Check if the key exists in the database
         if (results.length === 0) {
             return res.status(404).json({ error: 'Key not found' });
         }
-
         const keyInfo = results[0];
-
         // Get the email and used timestamp from the database results
         const { email, used_timestamp } = keyInfo;
-
         // Calculate the date when the key can be used again
         const currentDate = new Date();
         const keyLastUsedDate = new Date(used_timestamp);
         const timeDifference = currentDate.getTime() - keyLastUsedDate.getTime();
         const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const availableDate = new Date(keyLastUsedDate.getTime() + 14 * 24 * 60 * 60 * 1000);
-
         // Prepare the response data
         const responseData = {
             email,
             used_timestamp,
             available_date: daysDifference < 14 ? availableDate.toISOString().split('T')[0] : null
         };
-
         // Send the response
         res.status(200).json(responseData);
     });
 });
-
-
-
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
